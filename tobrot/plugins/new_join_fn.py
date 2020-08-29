0ff58956277e78bc6f723c06d11134483b5db749
svr666 <sreerajvr24@gmail.com>
2020-08-29 15:09:43 +0530

    update help msg

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -13,9 +13,19 @@ LOGGER = logging.getLogger(__name__)
 
 import pyrogram
 
-
 from tobrot import (
-    AUTH_CHANNEL
+    AUTH_CHANNEL,
+    LEECH_COMMAND,
+    YTDL_COMMAND,
+    GLEECH_COMMAND,
+    TELEGRAM_LEECH_COMMAND_G,
+    CANCEL_COMMAND_G,
+    GET_SIZE_G,
+    STATUS_COMMAND,
+    SAVE_THUMBNAIL,
+    CLEAR_THUMBNAIL,
+    PYTDL_COMMAND_G,
+    LOG_COMMAND
 )
 
 
@@ -37,8 +47,66 @@ async def help_message_f(client, message):
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
-    
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+
+    help_msg = f'''
+> Available Commands
+
+=> These commands should be used as a reply to a magnet link, a torrent link, or a direct link.
+
+=>> Uploads to telegram.
+
+/{LEECH_COMMAND}
+/{LEECH_COMMAND} archive
+/{LEECH_COMMAND} unzip
+/{LEECH_COMMAND} unrar
+/{LEECH_COMMAND} untar
+
+=>> Uploads to cloud.
+
+/{GLEECH_COMMAND}
+/{GLEECH_COMMAND} archive
+/{GLEECH_COMMAND} unzip
+/{GLEECH_COMMAND} unrar
+/{GLEECH_COMMAND} untar
+
+=> These commands should be used as a reply to a telegram file. Uploads telegram files to cloud.
+
+/{TELEGRAM_LEECH_COMMAND_G}
+/{TELEGRAM_LEECH_COMMAND_G} unzip
+/{TELEGRAM_LEECH_COMMAND_G} unrar
+/{TELEGRAM_LEECH_COMMAND_G} untar
+
+=> These commands should be used as a reply to a supported link.
+
+/{YTDL_COMMAND} : Uploads to tg.
+/{YTDL_COMMAND} gdrive : Upload to cloud.
+
+=> These commands should be used as a reply to a youtube playlist link.
+
+/{PYTDL_COMMAND_G} : Upload to telegram.
+/{PYTDL_COMMAND_G} gdrive : Upload to cloud.
+
+=> Extras
+
+/{SAVE_THUMBNAIL} : Set thumbnail(logo) in doc only mode too.
+
+/{CLEAR_THUMBNAIL} : Clear thumbnail(logo).
+
+/{GET_SIZE_G} : This will give you total size of your destination folder in cloud.
+
+/renewme : This will clear the remains of downloads which are not getting deleted after upload of the file or after /cancel command.
+
+/{STATUS_COMMAND} : Get the status message.
+
+/{CANCEL_COMMAND_G} : Cancel it !
+
+/{LOG_COMMAND} : Uploads the log.
+
+
+join this group for help -- @torrentleechgdrivesupport
+And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>
+'''
+    await message.reply_text(help_msg, disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
